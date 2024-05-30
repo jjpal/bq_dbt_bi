1d08b2de54a7e75bdbf0ae04ab41226120c06896
jjpal <36558688+jjpal@users.noreply.github.com>
2024-05-29 22:36:58 -0500

    added comma

--- bq_dbt_green_ec/models/staging/stg_reviews.sql
+++ bq_dbt_green_ec/models/staging/stg_reviews.sql
@@ -10,7 +10,7 @@ with src_reviews as (
 , renamed as (
     select
         review_id as survey_review_guid
-        user_id  as user_guid
+        , user_id  as user_guid
         , avg_rating as survey_rating
         , review as survey_review
         , review_date as survey_review_date
