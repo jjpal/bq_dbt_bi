6e997cdd7e450c73d70c42c4132e79657125962a
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-03 21:21:20 -0500

    updated rating alias

--- bq_dbt_green_ec/models/staging/stg_reviews.sql
+++ bq_dbt_green_ec/models/staging/stg_reviews.sql
@@ -11,7 +11,7 @@ with src_reviews as (
     select
         review_id as survey_review_guid
         , user_id  as user_guid
-        , avg_rating as survey_rating
+        , avg_rating as rating_review
         , review as survey_review
         , review_date as survey_review_date
     from src_reviews
