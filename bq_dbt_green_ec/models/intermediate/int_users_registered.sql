22f909c28aa14fb4cde118d022211201e5df7251
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-04 23:03:21 -0500

    updated age function for BQ

--- bq_dbt_green_ec/models/intermediate/int_users_registered.sql
+++ bq_dbt_green_ec/models/intermediate/int_users_registered.sql
@@ -4,10 +4,15 @@
     )
 }}
 
-select user_guid
-        , users_created_at_utc as user_registered_on
-        , coalesce(first_name, '') || ' '|| coalesce(last_name, '') as user_full_name
-        --, age(users_created_at_utc) as user_account_age --only postgres?
-        , date_diff(current_date(), users_created_at_utc, day) as user_account_age
+select 
+    user_guid
+    , users_created_at_utc as user_registered_on
+    , coalesce(first_name, '') || ' '|| coalesce(last_name, '') as user_full_name        
+    , timestamp_diff(current_timestamp(), user_registered_on, second) as user_account_age_sec
+    , timestamp_diff(current_timestamp(), user_registered_on, minute) as user_account_age_min
+    , timestamp_diff(current_timestamp(), user_registered_on, hour) as user_account_age_hr
+    , timestamp_diff(current_timestamp(), user_registered_on, day) as user_account_age_day
+    , timestamp_diff(current_timestamp(), user_registered_on, month) as user_account_age_month
+    , timestamp_diff(current_timestamp(), user_registered_on, year) as user_account_age_yr  
 from {{ ref('stg_users') }}
 order by user_account_age desc    
