c8c7d31efcdc5675a937d0a4516c3780938510ce
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-06 23:24:05 -0500

    update

--- bq_dbt_green_ec/models/staging/stg_users.sql
+++ bq_dbt_green_ec/models/staging/stg_users.sql
@@ -14,7 +14,7 @@ with src_users as (
         , last_name
         , email
         , phone_number
-        , created_at as users_created_at_utc
+        , created_at as user_created_at_utc
         , updated_at as updated_at_utc
         , address_id as address_guid 
     from src_users
