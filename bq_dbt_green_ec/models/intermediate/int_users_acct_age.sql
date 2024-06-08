cfe01ccf591fe2da1a48f079937e237b53e50a7c
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-07 23:27:36 -0500

    renamed

---
+++ bq_dbt_green_ec/models/intermediate/int_users_acct_age.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select 
+    user_guid
+    , coalesce(first_name, '') || ' '|| coalesce(last_name, '') as user_full_name 
+    , email as user_email
+    , phone_number as user_phone_number
+    , address_guid as user_address_guid       
+    , user_created_at_utc
+    , updated_at_utc
+    , timestamp_diff(current_timestamp(), user_created_at_utc, second) as user_account_age_sec
+    , timestamp_diff(current_timestamp(), user_created_at_utc, minute) as user_account_age_min
+    , timestamp_diff(current_timestamp(), user_created_at_utc, hour) as user_account_age_hr
+    , timestamp_diff(current_timestamp(), user_created_at_utc, day) as user_account_age_day
+    , timestamp_diff(current_timestamp(), user_created_at_utc, month) as user_account_age_month
+    , timestamp_diff(current_timestamp(), user_created_at_utc, year) as user_account_age_yr  
+from {{ ref('stg_users') }}
+order by user_account_age_yr desc    
