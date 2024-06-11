9c853720f42f28660151b6ebeb3c4e1bcaf071cb
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-10 23:35:11 -0500

    update

--- bq_dbt_green_ec/models/staging/stg_reviews.sql
+++ bq_dbt_green_ec/models/staging/stg_reviews.sql
@@ -10,8 +10,9 @@ with src_reviews as (
 , renamed as (
     select
         review_id as review_guid
-        , user_id  as user_guid
-        , avg_rating as rating_review
+        , user_id  as user_guid  
+        , product_id as product_guid         --
+        , rating as rating_review
         , review as survey_review
         , review_date as review_date
     from src_reviews
