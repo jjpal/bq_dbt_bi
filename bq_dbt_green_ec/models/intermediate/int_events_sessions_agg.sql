9633d25889c11ddc1e49c7c31fc903d5bb061612
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-14 23:48:23 -0500

    adding events_sessions_agg

---
+++ bq_dbt_green_ec/models/intermediate/int_events_sessions_agg.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+select 
+    session_guid
+    , min(events_created_at_utc) as session_start
+    , max(events_created_at_utc) as session_end
+    , user_guid      
+    , sum(case when event_type = 'page_view' then 1 else 0 end) as page_views
+    , sum(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts
+    , sum(case when event_type = 'checkout' then 1 else 0 end) as check_outs
+    , sum(case when event_type = 'package_shipped' then 1 else 0 end) as package_ships
+    , current_timestamp() as insertion_timestamp_ies
+from {{ ref('stg_greenery__events') }}
+group by 1, 4
