b40e74d40a2fc47d13fa98dd5895164ca0e98c08
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-17 23:52:06 -0500

    added guids

--- bq_dbt_green_ec/models/intermediate/int_events_sessions_agg.sql
+++ bq_dbt_green_ec/models/intermediate/int_events_sessions_agg.sql
@@ -4,8 +4,9 @@
   )
 }}
 
-select 
-    session_guid
+select
+    event_guid 
+    , session_guid
     , min(events_created_at_utc) as session_start
     , max(events_created_at_utc) as session_end
     , user_guid      
@@ -13,6 +14,8 @@ select
     , sum(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts
     , sum(case when event_type = 'checkout' then 1 else 0 end) as check_outs
     , sum(case when event_type = 'package_shipped' then 1 else 0 end) as package_ships
+    , order_guid
+    , product_guid
     , current_timestamp() as insertion_timestamp_ies
 from {{ ref('stg_events') }}
 group by 1, 4
