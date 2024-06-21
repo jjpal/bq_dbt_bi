6eedc88b0249b7db65504e12f2bb88c0c0ef6fec
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-20 23:45:19 -0500

    update

--- bq_dbt_green_ec/models/intermediate/int_reviews.sql
+++ bq_dbt_green_ec/models/intermediate/int_reviews.sql
@@ -5,11 +5,17 @@
 }}
 
 select 
-    review_guid
-    , user_guid  
-    , product_guid        
-    , rating_review
-    , survey_review
-    , review_date
+    revs.review_guid
+    , revs.user_guid  
+    , revs.product_guid        
+    , revs.rating_review
+    , revs.survey_review
+    , revs.review_date
+    , prods.product_name 
+    , prods.product_price
     , current_timestamp() as insertion_timestamp_ir
-from {{ ref('stg_reviews') }}
+from {{ ref('stg_reviews') }} as revs
+join {{ ref('stg_products') }} as prods
+    on revs.product_guid = prods.product_guid
+where product_guid is not null
+--group by 1,2,3,4,5,6,7,8
