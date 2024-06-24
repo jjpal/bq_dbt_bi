84db0dfd180021e5299ba4c60af72f05936d6116
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-23 23:02:36 -0500

    adding int_order_items

---
+++ bq_dbt_green_ec/models/intermediate/int_order_items.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+    o_items.order_guid
+    , o_iems.product_guid
+    , sum(o_items.quantity) as product_ordered
+    , prods.product_name    
+    , prods.product_price
+    , prods.product_inventory_amount
+    , current_timestamp() as insertion_timestamp_ioi 
+from {{ ref('stg_order_items') }} as o_items
+inner join {{ ref('stg_products') }} as prods
+on o_items.product_guid = prods.product_guid
+group by 1, 2, 4, 5, 6
+order by product_ordered desc  
+
