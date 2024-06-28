e702f50014729aedba581d7b95b1d2e4ede73321
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-27 23:33:12 -0500

    rename

---
+++ bq_dbt_green_ec/models/intermediate/int_orders_cnt_status_time_del.sql
@@ -0,0 +1,34 @@
+{{
+   config(
+      materialized='table'
+   )
+}}
+
+{%
+   set order_statuses = dbt_utils.get_query_results_as_dict(
+   "select distinct quote_literal(order_status) as order_status
+   , order_status as column_name from"
+   ~ ref ('stg_greenery__orders')
+   )
+%}
+
+select
+   order_guid
+   , user_guid
+   , address_guid
+   , count(order_guid) as number_of_orders
+   , orders_created_at_utc
+   {%- for order_status in order_statuses['order_status'] %}
+   , sum(case when order_status = {{ order_status }} then 1 else 0 end) 
+            as {{ order_statuses['column_name'][loop.index0] }} 
+   {%- endfor %} 
+   , avg(delivered_at_utc - orders_created_at_utc) as avg_delivery_time 
+   , {{ time_interval('delivered_at_utc', 'orders_created_at_utc')}} as time_delivery
+   , current_timestamp() as insertion_timestamp_ioa
+from {{ ref('stg_orders') }} as ords
+group by 1, 2, 3, 5, 8 
+order by time_delivery desc
+
+
+
+
