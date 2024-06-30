5d1f55484778f14da0f3264acb4186e3333da3fc
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-29 22:03:37 -0500

    update

--- bq_dbt_green_ec/models/intermediate/int_address_st_agg.sql
+++ bq_dbt_green_ec/models/intermediate/int_address_st_agg.sql
@@ -5,11 +5,11 @@
 }}
 
 select
-    address_guid as address_guid
-    , lower(trim(address)) as address
-    , zipcode as zipcode
-    , lower(trim(state)) as state
-    , lower(trim(country)) as country
+    address_guid
+    , lower(trim(address)) as user_address
+    , zipcode as user_zipcode
+    , lower(trim(state)) as user_state
+    , lower(trim(country)) as user_country
     , count(state) as state_count
     , current_timestamp() as insertion_timestamp_ia
 from {{ ref('stg_addresses') }}
