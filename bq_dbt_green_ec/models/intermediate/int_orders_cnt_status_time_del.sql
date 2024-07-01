4dc92d001267f9a9574cb534fa9628b6d6be23cd
jjpal <36558688+jjpal@users.noreply.github.com>
2024-06-30 22:32:24 -0500

    update

--- bq_dbt_green_ec/models/intermediate/int_orders_cnt_status_time_del.sql
+++ bq_dbt_green_ec/models/intermediate/int_orders_cnt_status_time_del.sql
@@ -6,9 +6,10 @@
 
 {%
    set order_statuses = dbt_utils.get_query_results_as_dict(
-   "select distinct quote_literal(order_status) as order_status
-   , order_status as column_name from"
-   ~ ref ('stg_orders')
+   "select 
+      distinct cast(order_status as string) as order_status,
+      order_status as column_name 
+   from " ~ ref('stg_orders')
    )
 %}
 
@@ -19,16 +20,16 @@ select
    , count(order_guid) as number_of_orders
    , orders_created_at_utc
    {%- for order_status in order_statuses['order_status'] %}
-   , sum(case when order_status = {{ order_status }} then 1 else 0 end) 
-      as {{ order_statuses['column_name'][loop.index0] }} 
+   , sum(case when order_status = {{ quote_literal(order_status) }} then 1 else 0 end) as {{ order_statuses['column_name'][loop.index0] }} 
    {%- endfor %} 
    , avg(delivered_at_utc - orders_created_at_utc) as avg_delivery_time 
-   , {{ time_interval('delivered_at_utc', 'orders_created_at_utc')}} as time_delivery
+   , {{ time_interval('delivered_at_utc', 'orders_created_at_utc') }} as time_delivery
    , current_timestamp() as insertion_timestamp_ioa
 from {{ ref('stg_orders') }} as ords
-group by 1, 2, 3, 5, 8 
+group by 
+   order_guid
+   , user_guid
+   , address_guid
+   , orders_created_at_utc
+   , delivered_at_utc
 order by time_delivery desc
-
-
-
-
