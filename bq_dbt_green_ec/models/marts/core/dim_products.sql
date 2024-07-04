61b32b02f2fa680f69bf13485c560887a0b3634f
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-03 23:33:24 -0500

    update

--- bq_dbt_green_ec/models/marts/core/dim_products.sql
+++ bq_dbt_green_ec/models/marts/core/dim_products.sql
@@ -8,11 +8,11 @@ select
     intp.product_guid
     , iprod.product_name
     , iprod.product_price
-    , iprod.product_inventory
+    , iprod.product_inventory_amount
     , iprod.product_ordered
     , events.session_guid as session_guid
     , events.page_url as product_url
     , current_timestamp() as insertion_timestamp_cdp
-from {{ ref('int_order_items_product_agg') }} iprod
-left join {{ ref('stg_greenery__events') }} events
+from {{ ref('int_order_items_prod_ord_agg') }} iprod
+left join {{ ref('stg_events') }} events
     on intp.product_guid = e.product_guid
