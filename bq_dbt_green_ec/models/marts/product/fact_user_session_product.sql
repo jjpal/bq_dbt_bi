7810bd629597b0d038698a5cb1c7a9cbe5e1b42c
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-10 23:27:27 -0500

    add

---
+++ bq_dbt_green_ec/models/marts/product/fact_user_session_product.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select 
+    ievent_s.product_guid
+    , iordi_pdo.product_name
+    , sum(ievent_s.add_to_cart) / count(distinct ievent_s.session_guid) as conver_rate_by_prod
+    , current_timestamp() as insertion_timestamp_fct_usp
+from {{ ref('int_events_sessions_agg') }} as ievent_s
+left join {{ ref('int_order_items_prod_ord_agg') }} as iordi_pdo
+on ievent_s.product_guid =  iordi_pdo.product_guid
+group by 1, 2
+order by conversion_rate_by_product desc 
