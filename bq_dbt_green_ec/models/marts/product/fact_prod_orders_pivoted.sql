b700d9b62b4f1ea2513e4dc41a5c0c99fdceb1fb
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-15 23:50:30 -0500

    update

--- bq_dbt_green_ec/models/marts/product/fact_prod_orders_pivoted.sql
+++ bq_dbt_green_ec/models/marts/product/fact_prod_orders_pivoted.sql
@@ -14,7 +14,8 @@ with orders_products as (
         , dprods.product_price
         , dprods.product_inventory_amount
         , ioi.items_ordered
-        , fo.created_at_utc
+        , fo.discount
+        , fo.orders_created_at_utc
         , current_timestamp() as insertion_timestamp_fpo
     from {{ ref('fact_orders')}} as fo
     left join {{ ref('int_order_items_prod_ord_agg')}} as ioi      
@@ -23,7 +24,7 @@ with orders_products as (
         on ioi.product_guid = dprods.product_guid
     left join {{ ref('dim_users')}} as dusers                       
         on fo.user_guid = dusers.user_guid
-    group by 1,2,3,4,5,6,7,8
+    group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     order by 1
 )
 ,
@@ -33,16 +34,17 @@ agg_pivot_product as (
         , op.product_name
         , sum(op.items_ordered) as units_sold
         , round(sum(op.product_price * op.items_ordered)) as expected_rev_no_disc_tax
-        , round(sum((op.product_price * op.items_ordered * (1 - op.discount)) * (1 + , str.combined_rate)), 2) as rev_with_tax_and_discount
-        , op.product_inventory_amount - coalesce(sum(op.items_ordered), 0) as current_inventory
-        , count(distinct op.order_guid) as times_ordered
+        , round(sum((op.product_price * op.items_ordered * (1 - op.discount)) * (1 + str.combined_rate)), 2) as rev_with_tax_and_discount
+        , op.product_inventory_amount as current_inventory
+        , op.product_inventory_amount - coalesce(sum(op.items_ordered), 0) as adjusted_inventory
+        , count(distinct op.order_guid) as num_times_ordered
         , count(distinct op.user_guid) as num_users_ordered
-        , max(op.created_at_utc) as last_ordered  
+        , max(op.orders_created_at_utc) as last_ordered  
     from orders_products as op  
     left join {{ ref('sales_tax_rates_us_st_local') }} as str 
-	    on op.state = str.state  
-    group by 1,2  
-    order by 2 
+	    on lower(trim(op.user_state)) = lower(trim(str.state)) 
+    group by op.product_guid, op.product_name, op.product_inventory_amount 
+    order by op.product_name 
 )
 
-select * from aggregated_and_pivoted_to_product
+select * from agg_pivot_product
