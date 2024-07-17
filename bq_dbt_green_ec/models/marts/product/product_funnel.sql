a58d3bab878d81869615c44aa7376aa695cc928e
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-16 23:09:20 -0500

    update

--- bq_dbt_green_ec/models/marts/product/product_funnel.sql
+++ bq_dbt_green_ec/models/marts/product/product_funnel.sql
@@ -4,7 +4,7 @@
     )
 }}
 
-with event_session_counts as(
+with event_session_counts as (
     select
         count(distinct (session_guid)) as total_sessions 
         , count(distinct ( case when page_view > 0 then session_guid end )) as page_views 
@@ -16,8 +16,9 @@ with event_session_counts as(
 
 select
     total_sessions 
-    , round((page_views::numeric / page_views:: numeric),2) || '%' as page_view_flow
-    , round((add_to_carts::numeric / page_views:: numeric),2) || '%' as view_cart_flow  
-    , round((checkouts::numeric / add_to_carts:: numeric),2) || '%' as  cart_checkout_flow 
-    , round((checkouts::numeric / page_views:: numeric),2) || '%' as page_view_checkout_flow
+    , concat(cast(round(page_views / page_views,2) as string), '%') as page_view_flow
+    , concat(cast(round(add_to_carts / page_views,2) as string), '%') as view_cart_flow  
+    , concat(cast(round(checkouts / add_to_carts,2) as string), '%') as cart_checkout_flow 
+    , concat(cast(round(checkouts / page_views,2) as string), '%') as page_view_checkout_flow
 from event_session_counts
+
