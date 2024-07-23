799ad912c7806932fd2ae76aaf35ac4cf16cd5fa
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-22 21:55:15 -0500

    update

--- bq_dbt_green_ec/models/marts/product/fact_user_session_product.sql
+++ bq_dbt_green_ec/models/marts/product/fact_user_session_product.sql
@@ -10,7 +10,7 @@ select
     , sum(ievent_s.add_to_carts) / count(distinct ievent_s.session_guid) as conver_rate_by_prod
     , current_timestamp() as insertion_timestamp_fct_usp
 from {{ ref('int_events_sessions_agg') }} as ievent_s
-left join {{ ref('int_order_items_prod_ord_agg') }} as iordi_pdo
+left join {{ ref('int_order_items_prod') }} as iordi_pdo
 on ievent_s.product_guid =  iordi_pdo.product_guid
 group by 1, 2
 order by conver_rate_by_prod desc 
