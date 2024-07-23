c675cdc2df354e9321fa82c0719ca38cf2be007c
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-22 22:32:16 -0500

    update

--- bq_dbt_green_ec/models/marts/product/product_funnel.sql
+++ bq_dbt_green_ec/models/marts/product/product_funnel.sql
@@ -16,9 +16,9 @@ with event_session_counts as (
 
 select
     total_sessions 
-    , concat(cast(round(page_views / page_views,2) as string), '%') as page_view_flow
-    , concat(cast(round(add_to_carts / page_views,2) as string), '%') as view_cart_flow  
-    , concat(cast(round(checkouts / add_to_carts,2) as string), '%') as cart_checkout_flow 
-    , concat(cast(round(checkouts / page_views,2) as string), '%') as page_view_checkout_flow
+    , cast(round(page_views / page_views,2) as numeric) as page_view_flow
+    , cast(round(add_to_carts / page_views,2) as numeric) as view_cart_flow  
+    , cast(round(checkouts / add_to_carts,2) as numeric) as cart_checkout_flow 
+    , cast(round(checkouts / page_views,2) as numeric) as page_view_checkout_flow
 from event_session_counts
 
