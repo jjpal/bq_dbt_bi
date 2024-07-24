891388d21ec0e3fc11b451b24b884d387d0fe93e
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-23 23:17:10 -0500

    update

--- bq_dbt_green_ec/models/marts/product/fact_prod_orders_pivoted.sql
+++ bq_dbt_green_ec/models/marts/product/fact_prod_orders_pivoted.sql
@@ -18,7 +18,7 @@ with orders_products as (
         , fo.orders_created_at_utc
         , current_timestamp() as insertion_timestamp_fpo
     from {{ ref('fact_orders')}} as fo
-    left join {{ ref('int_order_items_prod_ord_agg')}} as ioi      
+    left join {{ ref('int_order_items_prod')}} as ioi      
         on ioi.order_guid = fo.order_guid
     left join {{ ref('dim_products')}} as dprods                   
         on ioi.product_guid = dprods.product_guid
@@ -34,15 +34,12 @@ agg_pivot_product as (
         , op.product_name
         , sum(op.items_ordered) as units_sold
         , round(sum(op.product_price * op.items_ordered)) as expected_rev_no_disc_tax
-        , round(sum((op.product_price * op.items_ordered * (1 - op.discount)) * (1 + str.combined_rate)), 2) as rev_with_tax_and_discount
         , op.product_inventory_amount as current_inventory
         , op.product_inventory_amount - coalesce(sum(op.items_ordered), 0) as adjusted_inventory
         , count(distinct op.order_guid) as num_times_ordered
         , count(distinct op.user_guid) as num_users_ordered
         , max(op.orders_created_at_utc) as last_ordered  
     from orders_products as op  
-    left join {{ ref('sales_tax_rates_us_st_local') }} as str 
-	    on lower(trim(op.user_state)) = lower(trim(str.state)) 
     group by op.product_guid, op.product_name, op.product_inventory_amount 
     order by op.product_name 
 )
