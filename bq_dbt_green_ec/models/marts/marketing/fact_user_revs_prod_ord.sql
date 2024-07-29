8b0b911c33e53ffca0464a7756ed9a0310102abc
jjpal <36558688+jjpal@users.noreply.github.com>
2024-07-28 23:27:13 -0500

    add

---
+++ bq_dbt_green_ec/models/marts/marketing/fact_user_revs_prod_ord.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select 
+    fo.user_guid
+    , du.user_full_name
+    , du.user_email
+    , du.user_phone_number
+    , du.last_update_activity as user_last_update_activity
+    , du.user_account_age_day
+    , dr.product_name 
+    , dr.product_price
+    , dr.rating_review
+    , dr.survey_review
+    , dr.review_date
+    , fo.discount
+    , fo.promo_status
+    , fo.items_ordered
+    , fo.orders_created_at_utc   
+    , fo.product_inventory_amount
+    , current_timestamp() as insertion_timestamp_mfurpo
+from {{ ref('fact_orders')}} as fo
+left join {{ ref('dim_users')}} as du
+    on fo.user_guid = du.user_guid
+left join {{ ref('dim_reviews')}} as dr
+    on du.user_guid = dm.user_guid
+group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16        
+
