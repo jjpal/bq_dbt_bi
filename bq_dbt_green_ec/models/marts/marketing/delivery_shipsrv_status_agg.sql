e7c1e2f0460f2167dca352c80badd0ed3f097ef7
jjpal <36558688+jjpal@users.noreply.github.com>
2024-08-03 21:56:38 -0500

    update

--- bq_dbt_green_ec/models/marts/marketing/delivery_shipsrv_status_agg.sql
+++ bq_dbt_green_ec/models/marts/marketing/delivery_shipsrv_status_agg.sql
@@ -4,17 +4,17 @@
     )
 }}
 
-{%- set delivery_statuses = dbt_utils.get_column_values(
+{% set delivery_statuses = dbt_utils.get_column_values(
     table=ref('fact_orders'),
     column='delivery_status',
     where='delivery_status is not null'
-) -%}
+) 
+%}
 
-with delivery_status_agg as (
-
+with delivery_status_agg as(
     select 
         shipping_service
-        ,{%- for status in delivery_statuses %}
+        , {%- for status in delivery_statuses %}
         sum(case when delivery_status='{{status}}' then 1
             else 0 end) as num_delivery_{{status}}
         {%- if not loop.last %},{% endif -%}
