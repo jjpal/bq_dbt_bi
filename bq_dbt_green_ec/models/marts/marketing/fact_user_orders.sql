c75631db26f41a3f5f8fa9785427184e5c4a081b
jjpal <36558688+jjpal@users.noreply.github.com>
2024-08-03 21:55:52 -0500

    update

--- bq_dbt_green_ec/models/marts/marketing/fact_user_orders.sql
+++ bq_dbt_green_ec/models/marts/marketing/fact_user_orders.sql
@@ -11,9 +11,9 @@ select
     , fo.order_total
     , fo.tracking_guid
     , fo.shipping_service
-    , int_ordcstd.user_registered_on
-    , int_ordcstd.user_full_name
-    , int_ordcstd.user_account_age_day
+    , int_uaa.user_created_at_utc
+    , int_uaa.user_full_name
+    , int_uaa.user_account_age_day
     , du.user_email
     , du.user_phone_number
     , du.user_state
@@ -21,7 +21,7 @@ select
 from {{ ref('int_orders_cnt_status_time_del') }} as int_ordcstd 
 join {{ ref('fact_orders') }} as fo
     on int_ordcstd.user_guid = fo.user_guid
-join {{ ref('int_users_acct_age') }}
+join {{ ref('int_users_acct_age') }} as int_uaa
     on fo.user_guid = int_ordcstd.user_guid
 join {{ ref('dim_users') }} as du
     on fo.user_guid = du.user_guid
