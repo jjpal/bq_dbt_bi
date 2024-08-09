283ef0c160abee09793258bd36cd6bf8142e32e3
jjpal <36558688+jjpal@users.noreply.github.com>
2024-08-09 18:01:08 -0500

    update

--- bq_dbt_green_ec/models/marts/marketing/fact_user_orders.sql
+++ bq_dbt_green_ec/models/marts/marketing/fact_user_orders.sql
@@ -5,23 +5,24 @@
 }}
 
 select
-    int_ordcstd.user_guid
+    du.user_guid
+    , du.user_created_at_utc
+    , int_uaa.user_account_age_day   
+    , du.user_email
+    , du.user_phone_number
+    , du.user_address_guid
+    , du.user_state
     , int_ordcstd.order_count
-    , int_ordcstd.time_delivery
     , fo.order_total
     , fo.tracking_guid
     , fo.shipping_service
-    , int_uaa.user_created_at_utc
-    , int_uaa.user_full_name
-    , int_uaa.user_account_age_day
-    , du.user_email
-    , du.user_phone_number
-    , du.user_state
+    , int_ordcstd.time_delivery
     , current_timestamp() as insertion_timestamp_mfctuo  
 from {{ ref('int_orders_cnt_status_time_del') }} as int_ordcstd 
-join {{ ref('fact_orders') }} as fo
-    on int_ordcstd.user_guid = fo.user_guid
-join {{ ref('int_users_acct_age') }} as int_uaa
+inner join {{ ref('fact_orders') }} as fo
+    on int_ordcstd.order_guid = fo.order_guid
+inner join {{ ref('int_users_acct_age') }} as int_uaa
     on fo.user_guid = int_ordcstd.user_guid
-join {{ ref('dim_users') }} as du
-    on fo.user_guid = du.user_guid
+inner join {{ ref('dim_users') }} as du
+    on int_uaa.user_address_guid = du.user_address_guid
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13    
